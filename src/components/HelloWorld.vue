745eb71ec98dd595e66d032a59814140bea76beb
anpan0414 <1624379887@qq.com>
2020-06-28 10:32:52 +0800

    第一次修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>束带结发收快递费</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
