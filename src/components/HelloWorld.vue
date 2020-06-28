7cf137c82f22c2a08e9bbf81a64811012a6a3a64
anpan0414 <1624379887@qq.com>
2020-06-28 16:23:30 +0800

    mas

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,20 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>束带结发收快递费</h2>
-    <div>第一次分支。。。。。。</div>
-    <div>分支第二次变动</div>
-    <div>分支第三次变动</div>
-
-    <h2>奥斯卡加入一位UR围殴返回的是</h2>
-    <div>123</div>
-
-    <div>4 5 6</div>
-    <h2>阿速达soifUSdoifUS噢地士大夫索敌</h2>
-    <h1>塑料袋副科级时空来电</h1>
-
-    <div>塑料袋废旧塑料打开房间</div>
-    <div>1 2 3</div>
-    <div>为空容易忘ID要说的话是</div>
+    <div>first 第三方教室里打开发牢骚</div>
   </div>
 </template>
 
